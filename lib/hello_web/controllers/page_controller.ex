54675aff6417a3f8803dd75ada303f05ba1ddd80
Jingjing Duan <jingjing.duan@airbnb.com>
2017-09-26 22:45:48 -0700

    apply plugs only for certain actions

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,8 +1,15 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
+  plug :assign_welcome_message, "Welcome Back" when action [:index, :show]
+
   def index(conn, _params) do
-    render conn, "index.html"
+    # assign conveniently returns "conn"
+    conn
+    |> assign(:message, "Welcome Back!")
+    |> assign(:name, "Dweezil")
+    # both :message and :name will be available in the template
+    |> render conn, "index.html"
   end
 
   def show(conn, %{"id" => id}) do
@@ -23,5 +30,10 @@ defmodule HelloWeb.PageController do
          </body>
        </html>
       """
+  end
+
+  # assign a default message
+  defp assign_welcome_message(conn, msg) do
+    assign(conn, :message, msg)
   end
 end
