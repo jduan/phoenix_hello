b910bf5f574dc8db1bc4336db3f53df378518c36
Jingjing Duan <jingjing.duan@airbnb.com>
2017-10-02 23:22:45 -0700

    establish foreign keys

--- priv/repo/migrations/20171003060902_create_authors.exs
+++ priv/repo/migrations/20171003060902_create_authors.exs
@@ -6,7 +6,14 @@ defmodule Hello.Repo.Migrations.CreateAuthors do
       add :bio, :text
       add :role, :string
       add :genre, :string
-      add :user_id, references(:users, on_delete: :nothing)
+      # We used the :delete_all strategy again to enforce data integrity. This
+      # way, when a user is deleted from the application through
+      # Accounts.delete_user/1), we don’t have to rely on application code in
+      # our Accounts context to worry about cleaning up the CMS author records.
+      # This keeps our application code decoupled and the data integrity
+      # enforcement where it belongs – in the database.
+      add :user_id, references(:users, on_delete: :delete_all),
+        null: false
 
       timestamps()
     end
