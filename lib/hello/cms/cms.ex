3307c8affe97bbe51bb0ed3f1a63588e626f4d06
Jingjing Duan <jingjing.duan@airbnb.com>
2017-10-05 22:37:49 -0700

    increment page view

--- lib/hello/cms/cms.ex
+++ lib/hello/cms/cms.ex
@@ -81,6 +81,17 @@ defmodule Hello.CMS do
     |> Repo.update()
   end
 
+  def inc_page_views(%Page{} = page) do
+    {1, [%Page{views: views}]} =
+      Repo.update_all(
+        from(p in Page, where: p.id == ^page.id),
+        [inc: [views: 1]],
+        returning: [:views]
+      )
+
+    put_in(page.views, views)
+  end
+
   @doc """
   Deletes a Page.
 
