beb349c75ee261d2011c3e6474bd48eafa58fb8d
Jingjing Duan <jingjing.duan@airbnb.com>
2017-10-31 23:02:42 -0700

    change phoenix server ports

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :hello, HelloWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 8000],
   debug_errors: false,
   code_reloader: true,
   check_origin: false,
